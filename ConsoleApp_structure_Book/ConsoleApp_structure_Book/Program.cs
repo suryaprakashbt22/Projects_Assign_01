529ebc2584958e69a6b73d46c1dd5c00354d6d3c
Surya Prakash <suryaprakashbt1822@gmail.com>
2022-04-04 23:15:49 +0530

    Made use of new namespace library

--- ConsoleApp_structure_Book/ConsoleApp_structure_Book/Program.cs
+++ ConsoleApp_structure_Book/ConsoleApp_structure_Book/Program.cs
@@ -1,20 +1,21 @@
 ﻿
-//using ConsoleApp_structure_Book.Class1;
+using ConsoleApp_structure_Book;
 // See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Book Structure !");
 bool continueLoop = true;
 while (continueLoop)
 {
     Console.WriteLine("Enter the ID of the Book:");
 
-    ConsoleApp_structure_Book.BOOK c1 = new ConsoleApp_structure_Book.BOOK(); //Fully Qualified struct Name
-    
+    //ConsoleApp_structure_Book.BOOK c1 = new ConsoleApp_structure_Book.BOOK(); //Fully Qualified struct Name
+    BOOK c1=new BOOK();
     c1.bookID = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Enter the Title of the Book:");
     c1.Title = Console.ReadLine();
     Console.WriteLine("Enter the price of the Book:");
     c1.price = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Enter the type of Book\n\n");
+    Console.WriteLine("Enter the type of Book\n");
     Console.WriteLine("Press 1 for Magazine\nPress 2 for Novel\nPress 3 for Reference Book\nPress 4 for Miscellaneous\n");
     int choice = Convert.ToInt32(Console.ReadLine());
 
@@ -42,7 +43,7 @@ while (continueLoop)
         }
     }
     displayDetails();
-    Console.WriteLine("Press 0 to EXIT Program.\nPress 1 to Continue.");
+    Console.WriteLine("\nPress 0 to EXIT Program.\nPress 1 to Continue.");
     string s=Console.ReadLine();
     if (s == "0")
         continueLoop = false;
